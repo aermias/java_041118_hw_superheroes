2ea193d080e9eb7d87cf29a7722f4a299d8ea7fc
aermias <37191261+aermias@users.noreply.github.com>
2018-04-10 13:24:31 -0400

    Update TheFlash.java

--- src/main/java/superheroes/TheFlash.java
+++ src/main/java/superheroes/TheFlash.java
@@ -4,8 +4,4 @@ public class TheFlash extends SuperHero implements BlackHeroes {
     public TheFlash(String realName, String[] specialPowers) {
         super(realName, specialPowers);
     }
-
-    public String myBlackHero() {
-        return null;
-    }
 }
